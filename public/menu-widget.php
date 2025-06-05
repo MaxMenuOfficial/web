f953a26c85934e075650aaffb266fecb7bd52738
Ignacio Milanesi <maxmenu.com@maxmenu.com>
2025-06-05 19:44:30 +0200

    sdfghjh

--- public/menu-widget.php
+++ public/menu-widget.php
@@ -1,6 +1,5 @@
 <?php
 
-include '../get/get_domains.php';
 include '../get/get_restaurant_id.php';
 include '../get/get_logo.php';
 include '../get/get_idiomas.php'; 
@@ -15,6 +14,25 @@ include '../get/get_daily_menu.php';
 include '../get/get_traducciones.php';
 include '../get/get_alergenos.php';
 include '../get/get_colors.php';
+
+// Si quieres permitir CUALQUIER dominio (estándar para widgets públicos):
+header("Access-Control-Allow-Origin: *"); // <- Permite absolutamente cualquier origen
+
+// Si quieres permitir solo HTTPS y evitar localhost, puedes añadir un filtro:
+// $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+// if (stripos($origin, 'localhost') === false && stripos($origin, '127.0.0.1') === false) {
+//     header("Access-Control-Allow-Origin: $origin");
+// }
+
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Allow-Credentials: false'); // Debe ser false si Allow-Origin: *
+
+// Responder al preflight
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 ?>
 
