bc3344492495967935f46e9fea1fe576a6758be8
Ignacio Milanesi <maxmenu.com@maxmenu.com>
2025-07-11 20:19:29 +0200

    dfcgv

--- public/widget/widget.php
+++ public/widget/widget.php
@@ -1,22 +1,22 @@
 <?php
 // File: public/menu-widget.php
-header('Content-Type: text/html; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 
-include '../../get/get_restaurant_id.php';
-include '../../get/get_logo.php';
-include '../../get/get_idiomas.php'; 
-include '../../get/get_categoria.php';
-include '../../get/get_plataformas.php';
-include '../../get/get_restaurant_moneda.php';
-include '../../get/get_idiomas_for_items.php';
-include '../../get/get_simbolo_moneda.php';
-include '../../get/get_cat_and_subcat_for_item.php';
-include '../../get/get_brunch.php';
-include '../../get/get_daily_menu.php';
-include '../../get/get_traducciones.php';
-include '../../get/get_alergenos.php';
-include '../../get/get_colors.php'; 
+require_once __DIR__ . '/../../get/get_restaurant_id.php';
+require_once __DIR__ . '/../../get/get_logo.php';
+require_once __DIR__ . '/../../get/get_idiomas.php'; 
+require_once __DIR__ . '/../../get/get_categoria.php';
+require_once __DIR__ . '/../../get/get_plataformas.php';
+require_once __DIR__ . '/../../get/get_restaurant_moneda.php';
+require_once __DIR__ . '/../../get/get_idiomas_for_items.php';
+require_once __DIR__ . '/../../get/get_simbolo_moneda.php';
+require_once __DIR__ . '/../../get/get_cat_and_subcat_for_item.php';
+require_once __DIR__ . '/../../get/get_brunch.php';
+require_once __DIR__ . '/../../get/get_daily_menu.php';
+require_once __DIR__ . '/../../get/get_traducciones.php';
+require_once __DIR__ . '/../../get/get_alergenos.php';
+require_once __DIR__ . '/../../get/get_colors.php';
+
 
 ?>
 
