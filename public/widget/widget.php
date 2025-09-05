bece851707d0c7d92b92dcf03db3b6d8d17aaee6
Ignacio Milanesi <maxmenu.com@maxmenu.com>
2025-09-05 16:48:36 +0200

    sdfgh

--- public/widget/widget.php
+++ public/widget/widget.php
@@ -51,12 +51,12 @@ require_once __DIR__.'/../../get/get_tipografias.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="https://menu.maxmenu.com/assets/css/menu/styles/view-items.css">
-    <link rel="stylesheet" href="https://menu.maxmenu.com/assets/css/menu/styles/view-plataformas.css">
-    <link rel="stylesheet" href="https://menu.maxmenu.com/assets/css/menu/styles/view-logo.css">
-    <link rel="stylesheet" href="https://menu.maxmenu.com/assets/css/menu/styles/view-menu.css">
+    <link rel="stylesheet" href="https://menu.maxmenu.com/assets/css/widget/styles/view-items.css">
+    <link rel="stylesheet" href="https://menu.maxmenu.com/assets/css/widget/styles/view-plataformas.css">
+    <link rel="stylesheet" href="https://menu.maxmenu.com/assets/css/widget/styles/view-logo.css">
+    <link rel="stylesheet" href="https://menu.maxmenu.com/assets/css/widget/styles/view-menu.css">
 
-    
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cormorant+SC:wght@300;400;500;600;700&family=Lexend+Exa:wght@100..900&family=Marcellus+SC&family=Outfit:wght@100..900&family=Tangerine:wght@400;700&display=swap" rel="stylesheet">
